cd7de776aa354017dd097890ab5938abc8a3c553
Anton Pokydko <AntonPokydko@gmail.com>
2024-09-13 00:11:28 +0300

    generics 1&3 fix

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -3,7 +3,7 @@
 
 import axios from "axios";
 
-async function fetchData<Object>(url: string) {
+async function fetchData<T>(url: string): Promise<T> {
   try {
     const response = await axios.get(url);
     return response.data;
